a14fa57d7b4677a3d7d65a88914517fe4429e127
Peter John Arao <pitzzahh@gmail.com>
2025-02-18 12:53:13 +0800

    fix:Refine CSP directives for improved security

--- svelte-app/svelte.config.js
+++ svelte-app/svelte.config.js
@@ -14,16 +14,13 @@ const config = {
 			bundleStrategy: 'inline'
 		},
 		csp: {
-			mode: "nonce",
 			directives: {
-				"default-src": ["'self'", "*", "data:", "blob:", "'unsafe-inline'", "'unsafe-eval'"],
-				"style-src": ["'self'", "*", "'unsafe-inline'"],
-				"script-src": ["'self'", "*", "'unsafe-inline'", "'unsafe-eval'"],
-				"img-src": ["'self'", "*", "data:", "blob:", "avatars.githubusercontent.com"],
-				"font-src": ["'self'", "*", "data:"],
-				"connect-src": ["'self'", "*"],
-				"media-src": ["'self'", "*"]
-			},
+				'default-src': ['self', 'unsafe-inline'],
+				'script-src': ['self', 'unsafe-inline'],
+				'style-src': ['self', 'unsafe-inline'],
+				'img-src': ['self', 'data:', 'blob:'],
+				'font-src': ['self']
+			}
 		},
 		alias: {
 			"@/*": "./src/lib/*",
