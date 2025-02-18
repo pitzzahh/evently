b6f0a35ea7119c08d81bd0a3b9e0635d83a730f3
Peter John Arao <pitzzahh@gmail.com>
2025-02-18 22:47:09 +0800

    feat: Refactor load functions to include PocketBase superuser creation and server execution

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,17 +1,2 @@
-import { create_pocketbase_superuser, run_pocketbase_server } from "@/utils/db";
-import { getEnv } from "@/utils/invokes";
-import { error } from "@sveltejs/kit";
-
 export const prerender = true;
 export const ssr = false;
-
-export async function load() {
-  const email = await getEnv('ADMIN_EMAIL');
-  const pass = await getEnv('ADMIN_PASS');
-  console.log(`ADMIN_EMAIL: ${email}\nADMIN_PASS: ${pass}`);
-  if (!email || !pass) {
-    error(404, 'ADMIN_EMAIL or ADMIN_PASS not set in environment variables');
-  }
-  await run_pocketbase_server();
-  await create_pocketbase_superuser(email, pass);
-}
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,12 +1,18 @@
-import pb, { getAuth } from '$lib/db'
+import { create_pocketbase_superuser, run_pocketbase_server } from "@/utils/db";
+import { getEnv } from "@/utils/invokes";
+import { error } from "@sveltejs/kit";
 
 export async function load() {
-  await getAuth()
-
-  const notes = await pb.collection('notes').getFullList()
-  console.log('from page.ts load', notes);
+  const email = await getEnv('ADMIN_EMAIL');
+  const pass = await getEnv('ADMIN_PASS');
+  console.log(`ADMIN_EMAIL: ${email}\nADMIN_PASS: ${pass}`);
+  if (!email || !pass) {
+    error(404, 'ADMIN_EMAIL or ADMIN_PASS not set in environment variables');
+  }
+  await run_pocketbase_server();
+  await create_pocketbase_superuser(email, pass);
   return {
-    notes
+    notes: []
   }
 }
 
