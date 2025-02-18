6102562df61c7be066133fa0cc40582a662b826a
Peter John Arao <pitzzahh@gmail.com>
2025-02-18 13:02:52 +0800

    fix:Update PocketBase instance and authentication to use environment variables

--- svelte-app/src/lib/db.ts
+++ svelte-app/src/lib/db.ts
@@ -1,10 +1,9 @@
 import PocketBase from 'pocketbase'
 
-const pb = new PocketBase('http://127.0.0.1:8090')
-
+const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_INSTANCE)
 
 export async function getAuth() {
-  await pb.collection('_superusers').authWithPassword('admin@pocketbase.com', 'amiodarone')
+  await pb.collection('_superusers').authWithPassword(import.meta.env.VITE_ADMIN_EMAIL, import.meta.env.VITE_ADMIN_PASSWORD)
   console.log("Logged in to Pocket client: ", pb.authStore.isValid)
 }
 
