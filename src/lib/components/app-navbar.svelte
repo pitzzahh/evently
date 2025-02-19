34a577f7238ba4ff726acc03336c64270c573362
Alessandro Benig <alebenig4@gmail.com>
2025-02-19 15:05:25 +0800

    feat: integrate NavbarTime component into app navbar for dynamic time display

--- src/lib/components/app-navbar.svelte
+++ src/lib/components/app-navbar.svelte
@@ -3,6 +3,7 @@
 	import { page } from '$app/state';
 	import { cn } from '@/utils';
 	import { Ticket, Calendar } from 'lucide-svelte';
+	import NavbarTime from './navbar-time.svelte';
 
 	const isActive = (pathname: string) => page.url.pathname === pathname;
 	const routes = [
@@ -12,7 +13,7 @@
 </script>
 
 <header
-	class="bg-gray-1100/50 sticky z-10 top-0 flex h-auto w-full shrink-0 items-center justify-between gap-8 border-b bg-clip-padding px-4 py-3
+	class="bg-gray-1100/50 sticky top-0 z-10 flex h-auto w-full shrink-0 items-center justify-between gap-8 border-b bg-clip-padding px-4 py-3
  	backdrop-blur-md backdrop-filter transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 md:gap-0"
 >
 	<p class="text-lg font-bold">evently</p>
@@ -22,8 +23,8 @@
 			{#each routes as route}
 				<a href={route.pathname}>
 					<p
-						class={cn('flex items-center gap-2 font-semibold text-muted-foreground', {
-							'text-inherit': isActive(route.pathname)
+						class={cn('flex items-center gap-2 font-semibold text-muted-foreground opacity-50', {
+							'opacity-1 text-inherit': isActive(route.pathname)
 						})}
 					>
 						<route.icon class="size-6" />
@@ -33,8 +34,8 @@
 			{/each}
 		</div>
 		<div class="flex items-center gap-4">
-			<p class="text-sm font-semibold text-muted-foreground">9:08 AM GMT+8</p>
-			<Button>Create Event</Button>
+			<NavbarTime />
+			<Button href="/events/create">Create Event</Button>
 		</div>
 	</div>
 </header>
