45c9df1fd7cd8967d50bc351921ea5e8713c1d5d
Alessandro Benig <alebenig4@gmail.com>
2025-02-20 13:22:13 +0800

    feat: add event time picker component for start and end times

---
+++ src/routes/events/(components)/event-time-picker.svelte
@@ -0,0 +1,56 @@
+<div class="flex w-full gap-4">
+	<div class="flex w-full items-center gap-2 rounded-lg bg-gray-700/10 p-4 dark:bg-white/10">
+		<div class="flex flex-col items-center gap-1">
+			<div class="size-3 rounded-full border bg-gray-400"></div>
+			<div class="h-6 border-s-2 border-dashed border-gray-400"></div>
+			<div class="size-3 rounded-full border border-gray-400"></div>
+		</div>
+
+		<div class="flex w-full flex-col gap-1">
+			<!-- START -->
+			<div class="flex items-center justify-between">
+				<p class="text-sm text-muted-foreground">Start</p>
+				<div class="flex items-center gap-1">
+					<p class="rounded-bl-sm rounded-tl-sm bg-background p-2 text-sm">Thu, Feb 20</p>
+					<p class="rounded-br-sm rounded-tr-sm bg-background p-2 text-sm">12:30 PM</p>
+				</div>
+			</div>
+
+			<!-- END -->
+			<div class="flex items-center justify-between">
+				<p class="text-sm text-muted-foreground">End</p>
+				<div class="flex items-center gap-1">
+					<p class="rounded-bl-sm rounded-tl-sm bg-background p-2 text-sm">Thu, Feb 20</p>
+					<p class="rounded-br-sm rounded-tr-sm bg-background p-2 text-sm">12:30 PM</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="flex w-full items-center gap-2 rounded-lg bg-gray-700/10 p-4 dark:bg-white/10">
+		<div class="flex flex-col items-center gap-1">
+			<div class="size-3 rounded-full border bg-gray-400"></div>
+			<div class="h-6 border-s-2 border-dashed border-gray-400"></div>
+			<div class="size-3 rounded-full border border-gray-400"></div>
+		</div>
+
+		<div class="flex w-full flex-col gap-1">
+			<!-- START -->
+			<div class="flex items-center justify-between">
+				<p class="text-sm text-muted-foreground">Start</p>
+				<div class="flex items-center gap-1">
+					<p class="rounded-bl-sm rounded-tl-sm bg-background p-2 text-sm">Thu, Feb 20</p>
+					<p class="rounded-br-sm rounded-tr-sm bg-background p-2 text-sm">12:30 PM</p>
+				</div>
+			</div>
+
+			<!-- END -->
+			<div class="flex items-center justify-between">
+				<p class="text-sm text-muted-foreground">End</p>
+				<div class="flex items-center gap-1">
+					<p class="rounded-bl-sm rounded-tl-sm bg-background p-2 text-sm">Thu, Feb 20</p>
+					<p class="rounded-br-sm rounded-tr-sm bg-background p-2 text-sm">12:30 PM</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
