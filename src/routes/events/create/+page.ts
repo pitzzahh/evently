d66da98e20c3f641a409e769861d71fc60c5969a
Alessandro Benig <alebenig4@gmail.com>
2025-02-20 13:21:56 +0800

    feat: implement event creation form with validation

---
+++ src/routes/events/create/+page.ts
@@ -0,0 +1,9 @@
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { eventSchema } from '@/schema/event';
+
+export const load = async () => {
+	return {
+		event_form: await superValidate(zod(eventSchema))
+	};
+};
