e4616119ef001e7cea8ef40db4594fe4c73b23c5
Alessandro Benig <alebenig4@gmail.com>
2025-02-22 23:03:30 +0800

    feat: implement time selection with combo boxes for AM and PM in event time picker

--- src/routes/events/(components)/event-time-picker.svelte
+++ src/routes/events/(components)/event-time-picker.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import type { EventDateTime } from './event-form.svelte';
+	import TimeComboBox from './time-combo-box.svelte';
+	import { time_options } from '@/constants';
 
 	let { event_date }: { event_date: EventDateTime } = $props();
-	let formatted_date = $derived(formatDate(event_date.date));
+	let formatted_date = formatDate(event_date.date);
 
 	function formatDate(date: Date): string {
 		return date.toLocaleDateString('en-US', {
@@ -11,6 +13,23 @@
 			day: 'numeric'
 		});
 	}
+
+	// TODO: PUT THE UPDATED START AND END TIME IN THE ARRAY STATE OF EVENT DATES
+	let am_start_time = $state('8:00 AM');
+	let am_end_time = $state('12:00 PM');
+	let pm_start_time = $state('1:00 PM');
+	let pm_end_time = $state('5:00 PM');
+
+	function getFilteredEndTimes(startTime: string) {
+		const startIndex = time_options.indexOf(startTime);
+		return time_options.slice(startIndex + 1);
+	}
+
+	// automatically change the end time if theres some changes in start time
+	$effect(() => {
+		am_end_time = getFilteredEndTimes(am_start_time).at(0) as string;
+		pm_end_time = getFilteredEndTimes(pm_start_time).at(0) as string;
+	});
 </script>
 
 <div class="flex w-full gap-2">
@@ -18,41 +37,63 @@
 		{@render start_end_pattern()}
 
 		<div class="flex w-full flex-col gap-1">
-			<!-- START -->
-			{@render time_period({
-				title: 'AM Start',
-				date: formatted_date,
-				time: '12:30 PM'
-			})}
-			<!-- END -->
-			{@render time_period({
-				title: 'AM End',
-				date: formatted_date,
-				time: '12:30 PM'
-			})}
+			<!-- AM START -->
+			<div class="flex items-center justify-between">
+				<p class="text-sm text-muted-foreground">AM Start</p>
+				<div class="flex items-center gap-1">
+					<p class="w-[100px] rounded-bl-sm rounded-tl-sm bg-background p-2 text-center text-sm">
+						{formatted_date}
+					</p>
+					<TimeComboBox {time_options} bind:selected_time={am_start_time} />
+				</div>
+			</div>
+			<!-- AM END -->
+			<div class="flex items-center justify-between">
+				<p class="text-sm text-muted-foreground">AM End</p>
+
+				<div class="flex items-center gap-1">
+					<p class="w-[100px] rounded-bl-sm rounded-tl-sm bg-background p-2 text-center text-sm">
+						{formatted_date}
+					</p>
+					<TimeComboBox
+						time_options={getFilteredEndTimes(am_start_time)}
+						bind:selected_time={am_end_time}
+					/>
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="flex w-full items-center gap-2 rounded-lg bg-gray-700/10 p-4 dark:bg-white/10">
 		{@render start_end_pattern()}
 
 		<div class="flex w-full flex-col gap-1">
-			<!-- START -->
-			{@render time_period({ title: 'PM Start', date: formatted_date, time: '12:30 PM' })}
-			<!-- END -->
-			{@render time_period({ title: 'PM End', date: formatted_date, time: '12:30 PM' })}
+			<!-- PM START -->
+			<div class="flex items-center justify-between">
+				<p class="text-sm text-muted-foreground">PM Start</p>
+				<div class="flex items-center gap-1">
+					<p class="w-[100px] rounded-bl-sm rounded-tl-sm bg-background p-2 text-center text-sm">
+						{formatted_date}
+					</p>
+					<TimeComboBox {time_options} bind:selected_time={pm_start_time} />
+				</div>
+			</div>
+			<!-- PM END -->
+			<div class="flex items-center justify-between">
+				<p class="text-sm text-muted-foreground">PM End</p>
+
+				<div class="flex items-center gap-1">
+					<p class="w-[100px] rounded-bl-sm rounded-tl-sm bg-background p-2 text-center text-sm">
+						{formatted_date}
+					</p>
+					<TimeComboBox
+						time_options={getFilteredEndTimes(pm_start_time)}
+						bind:selected_time={pm_end_time}
+					/>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
-
-{#snippet time_period({ title, date, time }: { title: string; date: string; time: string })}
-	<div class="flex items-center justify-between">
-		<p class="text-sm text-muted-foreground">{title}</p>
-		<div class="flex items-center gap-1">
-			<p class="rounded-bl-sm rounded-tl-sm bg-background p-2 text-sm">{date}</p>
-			<p class="rounded-br-sm rounded-tr-sm bg-background p-2 text-sm">{time}</p>
-		</div>
-	</div>
-{/snippet}
 
 {#snippet start_end_pattern()}
 	<div class="flex flex-col items-center gap-1">
