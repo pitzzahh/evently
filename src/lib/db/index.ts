181a6e29a4988c3d740262e9eba3217289e93ad0
Peter John Arao <pitzzahh@gmail.com>
2025-02-24 11:44:51 +0800

    feat: initialize collections for AttendanceRecord, EventSchedule, EventDetails, Note, Participant, and QRCode models

---
+++ src/lib/db/index.ts
@@ -0,0 +1,8 @@
+import { AttendanceRecord, EventSchedule, EventDetails, Note, Participant , QRCode} from "./models";
+
+const ATTENDANCE_RECORDS_COLLECTION = new AttendanceRecord();
+const EVENT_SCHEDULE_COLLECTION = new EventSchedule();
+const EVENT_DETAILS_COLLECTION = new EventDetails();
+const NOTE_COLLECTION = new Note();
+const PARTICIPANT_COLLECTION = new Participant();
+const QRCODE_COLLECTION = new QRCode();
