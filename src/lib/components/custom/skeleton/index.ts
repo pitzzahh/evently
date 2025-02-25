54b36d3b2a655a41103b1b038beca410ca3ac72b
Peter John Arao <pitzzahh@gmail.com>
2025-02-25 22:03:40 +0800

    refactor: remove unused DataSkeleton and LogoSkeleton exports from skeleton index

--- src/lib/components/custom/skeleton/index.ts
+++ src/lib/components/custom/skeleton/index.ts
@@ -1,5 +1,3 @@
 export { default as CardSkeleton } from './card-skeleton.svelte';
-export { default as DataSkeleton } from './data-skeleton.svelte';
-export { default as LogoSkeleton } from './logo-skeleton.svelte';
 export { default as TableSkeleton } from './table-skeleton.svelte';
 export { default as SkeletonForm } from './skeleton-form.svelte';
