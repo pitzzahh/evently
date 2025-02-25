dc121e22ca8eb2e42bfdeaedacc74a34f8e217b8
Peter John Arao <pitzzahh@gmail.com>
2025-02-25 09:52:14 +0800

    feat: fetch and display events dynamically in event list component

--- src/routes/events/(components)/event-list.svelte
+++ src/routes/events/(components)/event-list.svelte
@@ -1,10 +1,28 @@
-<script>
+<script lang="ts">
 	import EventCard from './event-card.svelte';
 	import { Timeline } from 'svelte-vertical-timeline';
+	import { COLLECTIONS } from '@/db/index';
+	import type { EventDetails } from '@/db/models/types';
+
+	interface ComponentState {
+		events: EventDetails[];
+	}
+
+	let comp_state = $state<ComponentState>({
+		events: []
+	});
+
+	$effect(() => {
+		const cursor = COLLECTIONS.EVENT_DETAILS_COLLECTION.find({});
+		comp_state.events = cursor.fetch();
+		return () => {
+			cursor.cleanup();
+		};
+	});
 </script>
 
 <Timeline style="width: 100%;  padding: 0;">
-	{#each { length: 10 }}
-		<EventCard />
+	{#each comp_state.events as event}
+		<EventCard {...event} />
 	{/each}
 </Timeline>
