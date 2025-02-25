924133891c14c12b929d605c429d64f8bda19b90
Alessandro Benig <alebenig4@gmail.com>
2025-02-25 15:02:27 +0800

    feat: integrate superforms for participant addition in event load function

--- src/routes/events/[id]/+page.ts
+++ src/routes/events/[id]/+page.ts
@@ -1,5 +1,10 @@
-export function load({ params }) {
-  return {
-    event_id: params.id
-  }
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { add_participants_schema } from '@/schema/participant';
+
+export async function load({ params }) {
+	return {
+		event_id: params.id,
+		add_participants_form: await superValidate(zod(add_participants_schema))
+	};
 }
