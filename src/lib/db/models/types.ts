b5fae4302634e67798506286f91f217f2e799f7b
Peter John Arao <pitzzahh@gmail.com>
2025-02-25 15:10:15 +0800

    feat: remove unused Note type from models for cleaner code

--- src/lib/db/models/types.ts
+++ src/lib/db/models/types.ts
@@ -1,8 +1,7 @@
-import type { AttendanceRecordCollection, EventDetailsCollection, EventScheduleCollection, NoteCollection, ParticipantCollection, QRCodeCollection } from ".";
+import type { AttendanceRecordCollection, EventDetailsCollection, EventScheduleCollection, ParticipantCollection, QRCodeCollection } from ".";
 
 export type AttendanceRecord = Pick<AttendanceRecordCollection, 'id' | 'user_id' | 'event_id' | 'time_in' | 'time_out' | 'period' | 'created' | 'updated'>;
 export type EventSchedule = Pick<EventScheduleCollection, 'id' | 'event_id' | 'event_date' | 'am_start' | 'am_end' | 'pm_start' | 'pm_end' | 'created' | 'updated'>;
 export type EventDetails = Pick<EventDetailsCollection, 'id' | 'event_name' | 'type' | 'location' | 'description' | 'is_multi_day' | 'difference_in_days' | 'start_date' | 'end_date' | 'created' | 'updated'>;
-export type Note = Pick<NoteCollection, 'id' | 'title' | 'created' | 'updated'>;
 export type Participant = Pick<ParticipantCollection, 'id' | 'first_name' | 'middle_initial' | 'last_name' | 'email' | 'event_id' | 'created' | 'updated'>;
 export type QRCode = Pick<QRCodeCollection, 'id' | 'code' | 'created' | 'updated'>;
