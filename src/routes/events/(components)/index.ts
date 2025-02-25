9effb6e75b33485a13bda56e2cccc650eeef8221
Peter John Arao <pitzzahh@gmail.com>
2025-02-25 21:47:53 +0800

    refactor: remove unused event data table components and update index exports

--- src/routes/events/(components)/index.ts
+++ src/routes/events/(components)/index.ts
@@ -2,10 +2,6 @@ export { default as ParticipantDataTable } from "./(participant)/participant-dat
 export { default as ParticipantDataTableRowActions } from "./(participant)/participant-data-table-row-actions.svelte";
 export { default as ParticipantDataTableToolbar } from "./(participant)/participant-data-table-toolbar.svelte";
 
-export { default as EventDataTable } from "./(event)/event-data-table.svelte";
-export { default as EventDataTableRowActions } from "./(event)/event-data-table-row-actions.svelte";
-export { default as EventDataTableToolbar } from "./(event)/event-data-table-toolbar.svelte";
-
 export { default as EventTimePicker } from "./(event)/event-time-picker.svelte";
 export { default as EventCard } from "./(event)/event-card.svelte";
 export { default as EventForm } from "./(event)/event-form.svelte";
