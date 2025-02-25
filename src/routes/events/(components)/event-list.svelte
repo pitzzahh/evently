072633aa14d1347a17ce4d476acf869f797c9c81
Peter John Arao <pitzzahh@gmail.com>
2025-02-25 12:27:04 +0800

    feat: implement reactive loading for event details in event list

--- src/routes/events/(components)/event-list.svelte
+++ src/routes/events/(components)/event-list.svelte
@@ -3,6 +3,7 @@
 	import { Timeline } from 'svelte-vertical-timeline';
 	import { COLLECTIONS } from '@/db/index';
 	import type { EventDetails } from '@/db/models/types';
+	import { watch } from 'runed';
 
 	interface ComponentState {
 		events: EventDetails[];
@@ -18,23 +19,24 @@
 		events: []
 	});
 
-	$effect(() => {
-		const now = new Date();
-		const event_details = COLLECTIONS.EVENT_DETAILS_COLLECTION.find(
-			{
-				start_date: type === 'upcoming' ? { $gte: now } : { $lt: now }
-			},
-			{
-				sort: {
-					start_date: type === 'upcoming' ? 1 : -1
+	watch(
+		() => COLLECTIONS.EVENT_DETAILS_COLLECTION.isLoading,
+		() => {
+			const now = new Date();
+			const events = COLLECTIONS.EVENT_DETAILS_COLLECTION.find(
+				{
+					start_date: type === 'upcoming' ? { $gte: now } : { $lt: now }
+				},
+				{
+					sort: {
+						start_date: type === 'upcoming' ? 1 : -1
+					}
 				}
-			}
-		);
-		comp_state.events = event_details.fetch();
-		return () => {
-			event_details.cleanup();
-		};
-	});
+			);
+			comp_state.events = events.fetch();
+			console.log('comp_state.events', comp_state.events);
+		}
+	);
 </script>
 
 <Timeline style="width: 100%;  padding: 0;">
