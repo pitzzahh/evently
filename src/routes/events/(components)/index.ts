e4071f389f680430bd179ec7c72e921d34687025
Peter John Arao <pitzzahh@gmail.com>
2025-02-25 21:30:30 +0800

    feat: add event and participant components to event routes

--- src/routes/events/(components)/index.ts
+++ src/routes/events/(components)/index.ts
@@ -5,3 +5,11 @@ export { default as ParticipantDataTableToolbar } from "./(participant)/particip
 export { default as EventDataTable } from "./(event)/event-data-table.svelte";
 export { default as EventDataTableRowActions } from "./(event)/event-data-table-row-actions.svelte";
 export { default as EventDataTableToolbar } from "./(event)/event-data-table-toolbar.svelte";
+
+export { default as EventTimePicker } from "./(event)/event-time-picker.svelte";
+export { default as EventCard } from "./(event)/event-card.svelte";
+export { default as EventForm } from "./(event)/event-form.svelte";
+export { default as EventList } from "./(event)/event-list.svelte";
+
+export { default as ParticipantForm } from "./(participant)/participant-form.svelte";
+export { default as ParticipantDialog } from "./(participant)/participant-dialog.svelte";
