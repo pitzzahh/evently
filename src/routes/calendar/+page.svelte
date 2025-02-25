ce9922d89250c8787750266db047b1fccd2013ba
Peter John Arao <pitzzahh@gmail.com>
2025-02-25 14:49:34 +0800

    refactor: remove EventSchedule type from calendar component state and clean up initialization

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
 	import { EventCalendar } from '@routes/calendar/(components)';
 	import { scale } from 'svelte/transition';
-	import type { EventDetails, EventSchedule } from '@/db/models/types';
+	import type { EventDetails } from '@/db/models/types';
 	import { watch } from 'runed';
 	import { COLLECTIONS } from '@/db';
 
 	interface ComponentState {
 		event_details: EventDetails[];
-		event_schedules: EventSchedule[];
 	}
 
 	let comp_state = $state<ComponentState>({
-		event_details: [],
-		event_schedules: []
+		event_details: []
 	});
 
 	watch([() => COLLECTIONS.EVENT_DETAILS_COLLECTION.isLoading], () => {
@@ -21,6 +19,7 @@
 			{ fieldTracking: true }
 		);
 		comp_state.event_details = event_details_cursor.fetch();
+		console.log(comp_state.event_details);
 		return () => {
 			event_details_cursor.cleanup();
 		};
