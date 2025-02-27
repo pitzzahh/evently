45a5f2797fe973991993429a2b06bf61a768ef51
Alessandro Benig <alebenig4@gmail.com>
2025-02-27 20:02:17 +0800

    feat: enhance participant data table actions with edit, view, and remove functionalities

--- src/routes/events/(components)/(participant)/participant-data-table-row-actions.svelte
+++ src/routes/events/(components)/(participant)/participant-data-table-row-actions.svelte
@@ -1,38 +1,146 @@
 <script lang="ts">
-	import { Copy, Ellipsis } from '@/assets/icons';
+	import { Copy, Ellipsis, Pencil, View } from '@/assets/icons';
+	import { Trash } from 'lucide-svelte';
 	import type { Row } from '@tanstack/table-core';
 	import * as DropdownMenu from '@/components/ui/dropdown-menu/index.js';
 	import Button from '@/components/ui/button/button.svelte';
-	import { recursiveDropdown } from '@/components/custom/snippets';
 	import type { Participant } from '@/db/models/types';
+	import * as Dialog from '@/components/ui/dialog';
+	import EditParticipantForm from './edit-participant-form.svelte';
+	import type { SuperValidated } from 'sveltekit-superforms';
+	import type { ParticipantSchema } from '@/schema/participant';
+	import QR from '@svelte-put/qr/img/QR.svelte';
+	import ImgQR from '@svelte-put/qr/img/QR.svelte';
 
-	let { row }: { row: Row<Participant> } = $props();
+	let {
+		row,
+		participant_form
+	}: { row: Row<Participant>; participant_form: SuperValidated<ParticipantSchema> } = $props();
+	let comp_state = $state({
+		edit_open: false,
+		remove_open: false,
+		view_open: false
+	});
+
+	function handleOnOpenChange(value: boolean) {
+		if (comp_state.edit_open) {
+			comp_state.edit_open = value;
+		} else if (comp_state.remove_open) {
+			comp_state.remove_open = value;
+		} else {
+			comp_state.view_open = value;
+		}
+	}
+
+	let barcode = $state('');
+	let timeout = $state(0);
+
+	function handleKeydown(event: KeyboardEvent) {
+		if (event.key === 'Enter') {
+			event.preventDefault();
+			event.stopPropagation();
+			barcode = '';
+			return;
+		}
+
+		if (timeout) clearTimeout(timeout);
+
+		barcode = event.key;
+
+		timeout = setTimeout(() => {
+			barcode = '';
+		}, 500);
+	}
+
+	$effect(() => {
+		window.addEventListener('keydown', handleKeydown);
+
+		return () => {
+			window.removeEventListener('keydown', handleKeydown);
+		};
+	});
 </script>
 
-<DropdownMenu.Root>
-	<DropdownMenu.Trigger>
-		{#snippet child({ props })}
-			<Button {...props} variant="ghost" class="flex h-8 w-8 p-0 data-[state=open]:bg-muted">
-				<Ellipsis />
-				<span class="sr-only">Open Menu</span>
-			</Button>
-		{/snippet}
-	</DropdownMenu.Trigger>
-	<DropdownMenu.Content class="w-auto" align="end">
-		{@const current_archive = row.original}
-		<DropdownMenu.Separator />
-		<DropdownMenu.Sub>
-			<DropdownMenu.SubTrigger>
-				<Copy />
-				Copy</DropdownMenu.SubTrigger
+<Dialog.Dialog
+	open={comp_state.edit_open || comp_state.remove_open || comp_state.view_open}
+	onOpenChange={handleOnOpenChange}
+>
+	<DropdownMenu.Root>
+		<DropdownMenu.Trigger>
+			{#snippet child({ props })}
+				<Button {...props} variant="ghost" class="flex h-8 w-8 p-0 data-[state=open]:bg-muted">
+					<Ellipsis />
+					<span class="sr-only">Open Menu</span>
+				</Button>
+			{/snippet}
+		</DropdownMenu.Trigger>
+		<DropdownMenu.Content class="w-auto" align="end">
+			<DropdownMenu.Item onclick={() => (comp_state.view_open = true)}>
+				<View />
+				View
+			</DropdownMenu.Item>
+
+			<DropdownMenu.Item onclick={() => (comp_state.edit_open = true)}>
+				<Pencil />
+				Edit
+			</DropdownMenu.Item>
+
+			<DropdownMenu.Item
+				onclick={() => (comp_state.remove_open = true)}
+				class="text-red-600 hover:!bg-red-600/20 hover:!text-red-600"
 			>
-			<DropdownMenu.SubContent class="w-auto">
-				{#each Object.entries(current_archive) as [key, value]}
-					{@render recursiveDropdown({ value, key })}
-				{/each}
-			</DropdownMenu.SubContent>
-		</DropdownMenu.Sub>
-		<DropdownMenu.Separator />
-		<DropdownMenu.Separator />
-	</DropdownMenu.Content>
-</DropdownMenu.Root>
+				<Trash />
+				Remove
+			</DropdownMenu.Item>
+
+			<div></div>
+		</DropdownMenu.Content>
+	</DropdownMenu.Root>
+
+	{#if comp_state.view_open}
+		<Dialog.Content class="max-w-[650px]">
+			<Dialog.Header>
+				<Dialog.Title>Participant's Information</Dialog.Title>
+				<QR
+					data={row.original.id}
+					logoRatio={107 / 128}
+					shape="circle"
+					backgroundFill="white"
+					margin={4}
+					width="600"
+					height="600"
+				>
+					{#snippet img({ src })}
+						<img {src} alt="qr" class="size-[200px]" />
+					{/snippet}
+				</QR>
+				<p>{barcode}</p>
+			</Dialog.Header>
+		</Dialog.Content>
+	{/if}
+
+	{#if comp_state.edit_open}
+		<Dialog.Content class="max-w-[650px]">
+			<Dialog.Header>
+				<Dialog.Title>Edit Participant's Information</Dialog.Title>
+			</Dialog.Header>
+			<EditParticipantForm {participant_form} />
+		</Dialog.Content>
+	{/if}
+
+	{#if comp_state.remove_open}
+		<Dialog.Content>
+			<Dialog.Header>
+				<Dialog.Title>Remove Participant</Dialog.Title>
+				<Dialog.Description>
+					This action cannot be undone. Are you sure you want to permanently delete this
+					participant?
+				</Dialog.Description>
+			</Dialog.Header>
+			<Dialog.Footer>
+				<Button class="bg-red-600 hover:!bg-red-600/80">Delete</Button>
+				<Button variant="outline" onclick={() => (comp_state.remove_open = false)}>Cancel</Button>
+			</Dialog.Footer>
+		</Dialog.Content>
+	{/if}
+</Dialog.Dialog>
