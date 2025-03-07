cdce56278eeb3c5438570fee1e8fcedf9c4818d1
Alessandro Benig <alebenig4@gmail.com>
2025-03-07 08:45:24 +0800

    feat: enhance participant attendance columns with time tracking and additional fields

--- src/routes/events/(data)/columns.ts
+++ src/routes/events/(data)/columns.ts
@@ -6,10 +6,11 @@ import {
 	DataTableBadge
 } from '@/components/custom/data-table';
 import { ParticipantDataTableRowActions } from '@routes/events/(components)';
-import type { Participant } from '@/db/models/types';
-import { formatDateTime } from '@/utils/format';
+import type { Participant, ParticipantAttendance } from '@/db/models/types';
 import type { SuperValidated } from 'sveltekit-superforms';
 import type { ParticipantSchema } from '@/schema/participant';
+import { formatDateToTimeOption } from '@/utils/format';
+import TimeInOutCell from '../(components)/(participant)/time-in-out-cell.svelte';
 
 export function participantTableColumns(
 	participant_form: SuperValidated<ParticipantSchema>
@@ -96,35 +97,204 @@ export function participantTableColumns(
 					.includes(String(value ?? '').toLowerCase());
 			}
 		},
-		{
-			accessorKey: 'created',
-			header: ({ column }) =>
-				renderComponent(DataTableColumnHeader<Participant, unknown>, {
-					column,
-					title: 'Created At'
-				}),
-			cell: ({ row }) =>
-				renderComponent(DataTableBadge, {
-					variant: 'outline',
-					value: formatDateTime(row.original.created)
-				}),
-			filterFn: (row, id, value) => {
-				const date = new Date(row.getValue(id));
-				const searchValue = value.toLowerCase();
-				const dateStr = date.toLocaleDateString();
-				const timeStr = date.toLocaleTimeString();
-				const fullStr = date.toString().toLowerCase();
-				return (
-					dateStr.includes(searchValue) ||
-					timeStr.includes(searchValue) ||
-					fullStr.includes(searchValue)
-				);
-			}
-		},
+		// {
+		// 	accessorKey: 'created',
+		// 	header: ({ column }) =>
+		// 		renderComponent(DataTableColumnHeader<Participant, unknown>, {
+		// 			column,
+		// 			title: 'Created At'
+		// 		}),
+		// 	cell: ({ row }) =>
+		// 		renderComponent(DataTableBadge, {
+		// 			variant: 'outline',
+		// 			value: formatDateTime(row.original.created)
+		// 		}),
+		// 	filterFn: (row, id, value) => {
+		// 		const date = new Date(row.getValue(id));
+		// 		const searchValue = value.toLowerCase();
+		// 		const dateStr = date.toLocaleDateString();
+		// 		const timeStr = date.toLocaleTimeString();
+		// 		const fullStr = date.toString().toLowerCase();
+		// 		return (
+		// 			dateStr.includes(searchValue) ||
+		// 			timeStr.includes(searchValue) ||
+		// 			fullStr.includes(searchValue)
+		// 		);
+		// 	}
+		// },
 		{
 			id: 'actions',
 			header: () => 'Actions',
 			cell: ({ row }) => renderComponent(ParticipantDataTableRowActions, { participant_form, row })
 		}
+	];
+}
+
+export function participantAttendanceColumns(): ColumnDef<ParticipantAttendance>[] {
+	return [
+		// {
+		// 	id: 'select',
+		// 	header: ({ table }) =>
+		// 		renderComponent(DataTableCheckbox, {
+		// 			checked: table.getIsAllPageRowsSelected(),
+		// 			onCheckedChange: (value) => table.toggleAllPageRowsSelected(!!value),
+		// 			'aria-label': 'Select all',
+		// 			class: 'translate-y-[2px]'
+		// 		}),
+		// 	cell: ({ row }) =>
+		// 		renderComponent(DataTableCheckbox, {
+		// 			checked: row.getIsSelected(),
+		// 			onCheckedChange: (value) => row.toggleSelected(!!value),
+		// 			'aria-label': 'Select row',
+		// 			class: 'translate-y-[2px]'
+		// 		}),
+		// 	enableSorting: false,
+		// 	enableHiding: false
+		// },
+		{
+			accessorKey: 'first_name',
+			header: ({ column }) => {
+				return renderComponent(DataTableColumnHeader<ParticipantAttendance, unknown>, {
+					column,
+					title: 'First Name'
+				});
+			},
+			cell: ({ row }) => row.original.first_name,
+			filterFn: (row, id, value) => {
+				return String(row.getValue(id))
+					.toLowerCase()
+					.includes(String(value ?? '').toLowerCase());
+			}
+		},
+		{
+			accessorKey: 'middle_name',
+			header: ({ column }) =>
+				renderComponent(DataTableColumnHeader<ParticipantAttendance, unknown>, {
+					column,
+					title: 'Middle Name'
+				}),
+			cell: ({ row }) => {
+				return (
+					row.original.middle_name ||
+					renderComponent(DataTableBadge, {
+						variant: 'outline',
+						value: 'None'
+					})
+				);
+			},
+			filterFn: (row, id, value) => {
+				return String(row.getValue(id))
+					.toLowerCase()
+					.includes(String(value ?? '').toLowerCase());
+			}
+		},
+		{
+			accessorKey: 'last_name',
+			header: ({ column }) =>
+				renderComponent(DataTableColumnHeader<ParticipantAttendance, unknown>, {
+					column,
+					title: 'Last Name'
+				}),
+			cell: ({ row }) => row.original.last_name,
+			filterFn: (row, id, value) => {
+				return String(row.getValue(id))
+					.toLowerCase()
+					.includes(String(value ?? '').toLowerCase());
+			}
+		},
+		{
+			id: 'am',
+			header: ({ column }) =>
+				renderComponent(DataTableColumnHeader<ParticipantAttendance, unknown>, {
+					column,
+					title: 'AM Time',
+					class: 'text-xs'
+				}),
+			cell: ({ row }) => {
+				const attendance = row.original;
+
+				return renderComponent(TimeInOutCell, {
+					time_in: attendance.am_time_in ? formatDateToTimeOption(attendance.am_time_in) : '-:-',
+					time_out: attendance.am_time_out ? formatDateToTimeOption(attendance.am_time_out) : '-:-'
+				});
+			}
+		},
+
+		{
+			id: 'pm',
+			header: ({ column }) =>
+				renderComponent(DataTableColumnHeader<ParticipantAttendance, unknown>, {
+					column,
+					title: 'PM Time',
+					class: 'text-xs'
+				}),
+			cell: ({ row }) => {
+				const attendance = row.original;
+
+				return renderComponent(TimeInOutCell, {
+					time_in: attendance.pm_time_in ? formatDateToTimeOption(attendance.pm_time_in) : '-:-',
+					time_out: attendance.pm_time_out ? formatDateToTimeOption(attendance.pm_time_out) : '-:-'
+				});
+			}
+		},
+		// {
+		// 	accessorKey: 'email',
+		// 	header: ({ column }) =>
+		// 		renderComponent(DataTableColumnHeader<ParticipantAttendance, unknown>, {
+		// 			column,
+		// 			title: 'Email'
+		// 		}),
+		// 	cell: ({ row }) => row.original.email,
+		// 	filterFn: (row, id, value) => {
+		// 		return String(row.getValue(id))
+		// 			.toLowerCase()
+		// 			.includes(String(value ?? '').toLowerCase());
+		// 	}
+		// },
+		{
+			accessorKey: 'latest_time_scanned',
+			header: ({ column }) =>
+				renderComponent(DataTableColumnHeader<ParticipantAttendance, unknown>, {
+					column,
+					title: 'Latest Time Scanned'
+				}),
+
+			cell: ({ row }) => formatDateToTimeOption(row.original.latest_time_scanned),
+			filterFn: (row, id, value) => {
+				return String(row.getValue(id))
+					.toLowerCase()
+					.includes(String(value ?? '').toLowerCase());
+			}
+		}
+		// {
+		// 	accessorKey: 'created',
+		// 	header: ({ column }) =>
+		// 		renderComponent(DataTableColumnHeader<Participant, unknown>, {
+		// 			column,
+		// 			title: 'Created At'
+		// 		}),
+		// 	cell: ({ row }) =>
+		// 		renderComponent(DataTableBadge, {
+		// 			variant: 'outline',
+		// 			value: formatDateTime(row.original.created)
+		// 		}),
+		// 	filterFn: (row, id, value) => {
+		// 		const date = new Date(row.getValue(id));
+		// 		const searchValue = value.toLowerCase();
+		// 		const dateStr = date.toLocaleDateString();
+		// 		const timeStr = date.toLocaleTimeString();
+		// 		const fullStr = date.toString().toLowerCase();
+		// 		return (
+		// 			dateStr.includes(searchValue) ||
+		// 			timeStr.includes(searchValue) ||
+		// 			fullStr.includes(searchValue)
+		// 		);
+		// 	}
+		// },
+		// {
+		// 	id: 'actions',
+		// 	header: () => 'Actions',
+		// 	cell: ({ row }) => renderComponent(ParticipantDataTableRowActions, { participant_form, row })
+		// }
 	];
 }
