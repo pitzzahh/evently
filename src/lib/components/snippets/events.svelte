25c7e0f578bf85ffc0b9dbe09fdabad617892256
Alessandro Benig <alebenig4@gmail.com>
2025-03-11 10:01:30 +0800

    feat: enhance upcoming event styling for better visibility

--- src/lib/components/snippets/events.svelte
+++ src/lib/components/snippets/events.svelte
@@ -12,7 +12,8 @@
 		class={cn(
 			'rounded-lg border border-yellow-500 bg-yellow-500/20 text-sm font-semibold text-yellow-500 hover:!bg-yellow-500/20 hover:text-yellow-500',
 			{
-				'border-blue-500 bg-blue-500/20 text-blue-500 hover:!bg-blue-500/20': status === 'upcoming',
+				'border-blue-500 bg-blue-500/20 text-blue-500 hover:!bg-blue-500/20 hover:!text-blue-500':
+					status === 'upcoming',
 				'hover:!bg-[#00E200]30 border-[#00E200] bg-[#00E200]/20 text-[#009900] hover:!bg-[#00E200]/20 hover:!text-[#009900] dark:border-green-600 dark:bg-green-600/40 dark:text-[#0bb90b] dark:hover:!bg-green-600/40':
 					status === 'ongoing'
 			}
