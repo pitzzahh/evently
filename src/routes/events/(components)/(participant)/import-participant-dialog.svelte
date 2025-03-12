97c5043be1527bee084810011364fcd1e4a40931
Peter John Arao <pitzzahh@gmail.com>
2025-03-12 08:41:27 +0800

    fix: update file upload handling to support File objects and improve file management

--- src/routes/events/(components)/(participant)/import-participant-dialog.svelte
+++ src/routes/events/(components)/(participant)/import-participant-dialog.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { Button, buttonVariants } from '@/components/ui/button';
 	import * as Dialog from '@/components/ui/dialog';
-	import { Import, X, Sheet } from '@/assets/icons';
+	import { Import, X } from '@/assets/icons';
 	import {
+		displaySize,
 		FileDropZone,
 		MEGABYTE,
 		type FileDropZoneProps
@@ -14,6 +15,7 @@
 	import * as AlertDialog from '@/components/ui/alert-dialog';
 	import { readParticipants } from '@/utils/exports/excel';
 	import { COLLECTIONS } from '@/db';
+	import { onDestroy } from 'svelte';
 
 	interface ImportParticipantsDialogProps {
 		event_id: string;
@@ -21,56 +23,63 @@
 		open_add_participants_dialog?: boolean;
 	}
 
+	type UploadedFile = {
+		name: string;
+		type: string;
+		size: number;
+		uploadedAt: number;
+		url: Promise<string>;
+		file: File;
+	};
+
 	let {
 		event_id,
 		disabled = false,
 		open_add_participants_dialog = $bindable(false)
 	}: ImportParticipantsDialogProps = $props();
 
-	const onUpload: FileDropZoneProps['onUpload'] = async (filePaths) => {
-		await Promise.allSettled(filePaths.map((filePath) => uploadFile(filePath)));
+	const date = new SvelteDate();
+	let files = $state<UploadedFile[]>([]);
+
+	const onUpload: FileDropZoneProps['onUpload'] = async (files) => {
+		await Promise.allSettled(files.map((file) => uploadFile(file)));
 	};
 
 	const onFileRejected: FileDropZoneProps['onFileRejected'] = async ({ reason, file }) => {
-		toast.error(`${file} failed to upload!`, { description: reason });
+		toast.error(`${file.name} failed to upload!`, { description: reason });
 	};
 
-	const uploadFile = async (filePath: string) => {
+	const uploadFile = async (file: File) => {
 		// don't upload duplicate files
-		if (selected_file && selected_file.path === filePath) return;
+		if (files.find((f) => f.name === file.name)) return;
 
 		const urlPromise = new Promise<string>((resolve) => {
 			// add some fake loading time
-			sleep(1000).then(() => resolve(filePath));
+			sleep(1000).then(() => resolve(URL.createObjectURL(file)));
 		});
 
-		selected_file = {
-			path: filePath,
-			name: filePath.split('/').pop() ?? '',
-			uploadedAt: Date.now()
-		};
+		files.push({
+			name: file.name,
+			type: file.type,
+			size: file.size,
+			uploadedAt: Date.now(),
+			url: urlPromise,
+			file
+		});
 
 		// we await since we don't want the onUpload to be complete until the files are actually uploaded
 		await urlPromise;
 	};
 
-	type UploadedFile = {
-		path: string;
-		name: string;
-		uploadedAt: number;
-	};
-
-	let selected_file = $state<UploadedFile | null>(null);
-	let date = new SvelteDate();
-
 	async function handleImportParticipants() {
-		if (!selected_file) {
+		if (files.length === 0) {
 			toast.error('No files selected!');
 			return;
 		}
+		const [selected_file] = files;
 		console.log('selected_file', selected_file);
-		const file_url = selected_file.path;
-		const participants = await readParticipants(file_url, event_id);
+		const { file } = selected_file;
+		const participants = await readParticipants(file, event_id);
 
 		COLLECTIONS.PARTICIPANT_COLLECTION.insertMany(participants);
 
@@ -86,6 +95,12 @@
 			clearInterval(interval);
 		};
 	});
+
+	onDestroy(async () => {
+		for (const file of files) {
+			URL.revokeObjectURL(await file.url);
+		}
+	});
 </script>
 
 <Dialog.Root bind:open={open_add_participants_dialog}>
@@ -93,6 +108,7 @@
 		><Import class="size-4" />Import Excel Participants</Dialog.Trigger
 	>
 	<Dialog.Content class="max-w-[750px]">
+		{@const no_file = files.length === 0}
 		<Dialog.Header>
 			<Dialog.Title>Importing guide</Dialog.Title>
 			<Dialog.Description>
@@ -129,42 +145,45 @@
 					maxFileSize={10 * MEGABYTE}
 					accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 				/>
-				{#if selected_file}
+				{#if no_file}
 					<div class="flex flex-col gap-2">
-						<div class="flex place-items-center justify-between gap-2">
-							<div class="flex place-items-center gap-2 whitespace-nowrap">
-								{#await selected_file.path then src}
-									<div class="relative size-9 overflow-clip">
-										<Sheet
-											class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 overflow-clip"
-										/>
+						{#each files as file, i (file.name)}
+							<div class="flex place-items-center justify-between gap-2">
+								<div class="flex place-items-center gap-2">
+									{#await file.url then src}
+										<div class="relative size-9 overflow-clip">
+											<img
+												{src}
+												alt={file.name}
+												class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 overflow-clip"
+											/>
+										</div>
+									{/await}
+									<div class="flex flex-col">
+										<span>{file.name}</span>
+										<span class="text-xs text-muted-foreground">{displaySize(file.size)}</span>
 									</div>
-								{/await}
-								<div class="flex flex-col">
-									<span>{selected_file.name}</span>
-									<!-- <span class="text-xs text-muted-foreground"
-										>{displaySize(selected_file.size)}</span
-									> -->
 								</div>
+								{#await file.url}
+									<Progress
+										class="h-2 w-full flex-grow"
+										value={((date.getTime() - file.uploadedAt) / 1000) * 100}
+										max={100}
+									/>
+								{:then url}
+									<Button
+										variant="outline"
+										size="icon"
+										onclick={() => {
+											URL.revokeObjectURL(url);
+											files = [...files.slice(0, i), ...files.slice(i + 1)];
+										}}
+									>
+										<X />
+									</Button>
+								{/await}
 							</div>
-							{#await selected_file.path}
-								<Progress
-									class="h-2 w-full flex-grow"
-									value={((date.getTime() - selected_file.uploadedAt) / 1000) * 100}
-									max={100}
-								/>
-							{:then path}
-								<Button
-									variant="outline"
-									size="icon"
-									onclick={() => {
-										selected_file = null;
-									}}
-								>
-									<X />
-								</Button>
-							{/await}
-						</div>
+						{/each}
 					</div>
 				{/if}
 			</div></Dialog.Header
@@ -172,9 +191,9 @@
 		<Dialog.Footer>
 			<AlertDialog.Root>
 				<AlertDialog.Trigger
-					disabled={!selected_file}
+					disabled={no_file}
 					class={cn(buttonVariants({ variant: 'outline', className: 'w-full' }), {
-						'cursor-not-allowed': !selected_file
+						'cursor-not-allowed': no_file
 					})}
 				>
 					Import Participants
