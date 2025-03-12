dd4336f32efe768981b9ca55da131c0726564bcd
Peter John Arao <pitzzahh@gmail.com>
2025-03-12 14:22:44 +0800

    feat: update onmessage handler to use WebWorkerDocumentMetaDetails type for improved type safety

--- src/lib/workers/generate-daily-attendance.worker.ts
+++ src/lib/workers/generate-daily-attendance.worker.ts
@@ -1,12 +1,8 @@
 import type { DocumentMetaDetails } from "@/types/exports";
+import type { WebWorkerDocumentMetaDetails } from "@/types/workers";
 import { generateDailyAttendanceReportPDF } from "@/utils/exports/pdf";
 
-export type ModifiedDocumentMetaDetails = Omit<DocumentMetaDetails, 'event_details' | 'participants'> & {
-  event_details: string;
-  participants: string;
-};
-
-onmessage = async (message: MessageEvent<ModifiedDocumentMetaDetails>) => {
+onmessage = async (message: MessageEvent<WebWorkerDocumentMetaDetails>) => {
   postMessage(await generateDailyAttendanceReportPDF({
     ...message.data,
     event_details: JSON.parse(message.data.event_details) as DocumentMetaDetails['event_details'],
