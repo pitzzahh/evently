e3720433b4f5774aca19213e6bf8b943b7e9ed2d
Peter John Arao <pitzzahh@gmail.com>
2025-03-12 08:13:48 +0800

    fix: update participant import logic to use selected file path

--- src/routes/events/(components)/(participant)/import-participant-dialog.svelte
+++ src/routes/events/(components)/(participant)/import-participant-dialog.svelte
@@ -70,10 +70,7 @@
 		}
 		console.log('selected_file', selected_file);
 		const file_url = selected_file.path;
-		const participants = await readParticipants(
-			'C:\\Users\\peter\\Downloads\\MOCK_DATA(2).xlsx',
-			event_id
-		);
+		const participants = await readParticipants(file_url, event_id);
 
 		COLLECTIONS.PARTICIPANT_COLLECTION.insertMany(participants);
 
