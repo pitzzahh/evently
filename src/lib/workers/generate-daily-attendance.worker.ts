5c4beb1e7fd7520f9788f97c2562be711db87c92
Peter John Arao <pitzzahh@gmail.com>
2025-03-12 14:05:02 +0800

    feat: modify daily attendance report worker to handle structured event details and participants

--- src/lib/workers/generate-daily-attendance.worker.ts
+++ src/lib/workers/generate-daily-attendance.worker.ts
@@ -1,9 +1,20 @@
 import type { DocumentMetaDetails } from "@/types/exports";
 import { generateDailyAttendanceReportPDF } from "@/utils/exports/pdf";
 
-onmessage = (message: MessageEvent<DocumentMetaDetails>) => {
-  console.log(message);
-  postMessage(generateDailyAttendanceReportPDF(message.data));
+type ModifiedDocumentMetaDetails = Omit<DocumentMetaDetails, 'event_details' | 'participants'> & {
+  event_details: string;
+  participants: string;
+};
+
+onmessage = (message: MessageEvent<ModifiedDocumentMetaDetails>) => {
+  console.log(JSON.stringify(message, null, 2));
+  const data = {
+    ...message.data,
+    event_details: JSON.parse(message.data.event_details) as DocumentMetaDetails['event_details'],
+    participants: JSON.parse(message.data.participants) as DocumentMetaDetails['participants'],
+  } as DocumentMetaDetails;
+  console.log(data);
+  postMessage(generateDailyAttendanceReportPDF(data));
 };
 
 export { };
