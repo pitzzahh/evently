498ce417f39cc06fc3c0547c603d069ccec08b86
Peter John Arao <pitzzahh@gmail.com>
2025-03-12 10:29:38 +0800

    feat: add FileOutput icon to the icon library

--- src/lib/assets/icons/index.ts
+++ src/lib/assets/icons/index.ts
@@ -36,3 +36,4 @@ export { default as Trash } from 'lucide-svelte/icons/trash';
 export { default as Import } from 'lucide-svelte/icons/import';
 export { default as Download } from 'lucide-svelte/icons/download';
 export { default as Sheet } from 'lucide-svelte/icons/sheet';
+export { default as FileOutput } from 'lucide-svelte/icons/file-output';
