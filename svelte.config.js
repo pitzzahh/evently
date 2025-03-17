7af19584b4cf8cac27fff53761aff3ce79042e27
Peter John Arao <pitzzahh@gmail.com>
2025-03-17 14:09:04 +0800

    feat: update CSP directives in svelte.config.js to allow connections from localhost and tauri.localhost

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,10 @@ const config = {
 		},
 		csp: {
 			directives: {
-				'script-src': ['self', 'unsafe-inline'],
-				'style-src': ['self', 'unsafe-inline'],
+				'script-src': ['self', 'unsafe-inline', 'http://tauri.localhost', 'http://localhost'],
+				'connect-src': ['self', 'unsafe-inline', 'unsafe-eval', 'http://localhost'],
+				'img-src': ['self', 'data:'],
+				'style-src': ['self', 'unsafe-inline', 'http://localhost', 'http://tauri.localhost'],
 			}
 		}
 	}
