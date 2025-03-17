6a0c572393f6ad0b2781505befbd81b4573022b3
Alessandro Benig <alebenig4@gmail.com>
2025-03-17 10:04:49 +0800

    feat: add QR code scanner dialog component and integrate into participant page

---
+++ src/routes/events/(components)/(participant)/qr-code-scanner-dialog.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { buttonVariants } from '@/components/ui/button';
+	import * as Dialog from '@/components/ui/dialog';
+	import { Camera } from 'lucide-svelte';
+	import QrCodeScanner from '@/components/custom/qr-code-scanner/qr-code-scanner.svelte';
+
+	let { handleScan }: { handleScan: (data: string) => void } = $props();
+</script>
+
+<Dialog.Root>
+	<Dialog.Trigger
+		class={buttonVariants({ class: 'rounded-lg border px-4 py-3 text-sm', variant: 'outline' })}
+	>
+		<Camera class="size-4" />
+		Scan with Camera
+	</Dialog.Trigger>
+	<Dialog.Content class="">
+		<Dialog.Header>
+			<Dialog.Title>Scan Participant</Dialog.Title>
+		</Dialog.Header>
+		<QrCodeScanner onDetect={(data) => handleScan(data)} />
+	</Dialog.Content>
+</Dialog.Root>
