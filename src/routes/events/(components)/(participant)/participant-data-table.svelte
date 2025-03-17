301c0822d7395a91898716b94843d19693e3d506
Peter John Arao <pitzzahh@gmail.com>
2025-03-17 09:47:34 +0800

    refactor: include event details in participant data table for improved context

--- src/routes/events/(components)/(participant)/participant-data-table.svelte
+++ src/routes/events/(components)/(participant)/participant-data-table.svelte
@@ -2,8 +2,7 @@
 	import { DataTable } from '@/components/custom/data-table';
 	import { participantTableColumns } from '@routes/events/(data)/columns';
 	import { ParticipantDataTableToolbar } from '..';
-	import { generateOptions } from '@/utils/filter';
-	import type { Participant } from '@/db/models/types';
+	import type { EventDetails, Participant } from '@/db/models/types';
 	import type { ParticipantSchema } from '@/schema/participant';
 	import type { SuperValidated } from 'sveltekit-superforms';
 	import ParticipantFloatingBar from './participant-floating-bar.svelte';
@@ -12,12 +11,17 @@
 		participants: Participant[];
 		participant_form: SuperValidated<ParticipantSchema>;
 		event_status: 'finished' | 'ongoing' | 'upcoming';
+		event_details: EventDetails;
 	}
 
-	let { participants, participant_form, event_status }: ParticipantDataTableProps = $props();
+	let { participants, participant_form, event_status, event_details }: ParticipantDataTableProps =
+		$props();
 </script>
 
-<DataTable data={participants} columns={participantTableColumns(participant_form, event_status)}>
+<DataTable
+	data={participants}
+	columns={participantTableColumns(participant_form, event_status, event_details)}
+>
 	{#snippet data_table_toolbar({ table })}
 		<ParticipantDataTableToolbar {table} />
 	{/snippet}
