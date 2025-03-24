dab1cab3247062e73bcdb2708711578eefdb5f03
Peter John Arao <pitzzahh@gmail.com>
2025-03-24 21:36:32 +0800

    move: new-app-ver-alert code to updater.svelte

--- src/lib/components/custom/updater/updater.svelte
+++ src/lib/components/custom/updater/updater.svelte
@@ -13,8 +13,10 @@
 </script>
 
 <script lang="ts">
-	import { Button } from '@/components/ui/button';
-	import { X } from 'lucide-svelte';
+	import * as Alert from '$lib/components/ui/alert';
+	import { AlertCircle, RefreshCw, X } from '@/assets/icons';
+	import * as Tooltip from '$lib/components/ui/tooltip';
+	import { Button, buttonVariants } from '@/components/ui/button';
 
 	let { title, dismiss = $bindable(false), description }: UpdaterProps = $props();
 
@@ -23,25 +25,46 @@
 	});
 </script>
 
-<section class="w-full border-b bg-white px-4 py-3 shadow-sm">
-	<div class="flex items-center justify-between gap-2">
-		<div class="flex-1 text-center">
-			<span class="text-sm">
-				<span class="font-medium">{title}</span>
-				<span class="text-muted-foreground">
-					{description}
-					.
-				</span>
-			</span>
+<Alert.Root
+	variant="default"
+	class="flex flex-col justify-between border-amber-500 bg-amber-50 dark:border-amber-700 dark:bg-amber-500/5"
+>
+	<div class="flex items-center justify-between">
+		<div class="flex items-center gap-1">
+			<AlertCircle class="mb-1.5 size-4 text-amber-800 dark:text-amber-300" />
+			<Alert.Title class="text-base text-amber-800 dark:text-amber-300"
+				>Update Available!</Alert.Title
+			>
 		</div>
+		<Tooltip.Provider>
+			<Tooltip.Root>
+				<Tooltip.Trigger
+					onclick={() => (dismiss = !dismiss)}
+					class={buttonVariants({
+						variant: 'outline',
+						size: 'sm',
+						className:
+							'h-6 w-6 -translate-y-2.5 translate-x-2.5 border-amber-500 p-0 text-amber-700 hover:bg-amber-100 dark:text-amber-200 dark:hover:bg-amber-900 dark:hover:text-amber-100'
+					})}
+				>
+					<X class="h-3 w-3" />
+				</Tooltip.Trigger>
+				<Tooltip.Content>
+					<p>Add to library</p>
+				</Tooltip.Content>
+			</Tooltip.Root>
+		</Tooltip.Provider>
+	</div>
 
+	<div class="flex items-center justify-between">
+		<Alert.Description>A new version 1.23.4 of the application is available.</Alert.Description>
 		<Button
-			variant="ghost"
-			size="icon"
-			class="-mr-2 h-8 w-8 flex-none"
-			onclick={() => (dismiss = true)}
+			size="sm"
+			variant="outline"
+			class="border-amber-500 text-amber-700 hover:bg-amber-100 dark:text-amber-200 dark:hover:bg-amber-900 dark:hover:text-amber-100"
 		>
-			<X class="h-4 w-4" />
+			<RefreshCw className="mr-2 h-4 w-4" />
+			Update Now
 		</Button>
 	</div>
-</section>
+</Alert.Root>
