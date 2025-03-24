801cb7146d370a6dc75c756124ca3a17624e9b02
Peter John Arao <pitzzahh@gmail.com>
2025-03-24 20:33:37 +0800

    remove: current changes to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,18 +6,6 @@
 	import { toast, Toaster } from 'svelte-sonner';
 	import AppNavbar from '@/components/app-navbar.svelte';
 	import { getCurrentWindow } from '@tauri-apps/api/window';
-	import type { Update } from '@tauri-apps/plugin-updater';
-	import { Updater } from '@/components/custom/updater';
-
-	interface ComponentState {
-		update: Update | null;
-		dismiss_update_notification: boolean;
-	}
-
-	let comp_state = $state<ComponentState>({
-		update: null,
-		dismiss_update_notification: false
-	});
 
 	let { children } = $props();
 </script>
@@ -49,19 +37,6 @@
 {#if dev}
 	<RenderScan />
 {/if}
-
-{#if comp_state.update && !comp_state.dismiss_update_notification}
-	<Updater
-		title="Update Available"
-		description="A new version of the app is available. Please restart the app to update."
-		bind:dismiss={comp_state.dismiss_update_notification}
-	/>
-{/if}
-<Updater
-	title="Update Available"
-	description="A new version of the app is available. Please restart the app to update."
-	bind:dismiss={comp_state.dismiss_update_notification}
-/>
 <AppNavbar />
 <div class="flex size-full justify-center">
 	<div class="flex flex-1 flex-col gap-4 p-4 pt-8 md:max-w-[80%] md:px-0">
