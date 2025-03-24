57fa8ea0b099dcffd9036b9778edaa80155f9f95
Peter John Arao <pitzzahh@gmail.com>
2025-03-24 22:35:30 +0800

    refactor: remove toast notifications from update check and installation functions

--- src/lib/utils/update/index.ts
+++ src/lib/utils/update/index.ts
@@ -1,13 +1,11 @@
 import { check, Update } from '@tauri-apps/plugin-updater';
 import { relaunch } from '@tauri-apps/plugin-process';
-import { toast } from 'svelte-sonner';
 
 /**
  * Check for updates and return the update object if available.
  * @returns {Promise<Update | null>} The update object or null if no update is available.
  */
 export async function checkForUpdates(): Promise<Update | null> {
-  toast.loading('Checking for updates...');
   return await check();
 }
 
@@ -30,14 +28,11 @@ export async function installUpdate(update: Update, _relaunch: boolean, _progres
         _progress && _progress((downloaded / contentLength) * 100);
         break;
       case 'Finished':
-        _onFinished?.();
         break;
     }
   });
   if (_relaunch) {
-    toast.info('Update installed successfully', {
-      description: 'The application will now restart to apply the changes.',
-    });
+    _onFinished?.();
     await relaunch();
   }
 }
