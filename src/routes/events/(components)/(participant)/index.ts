2602c9b4939249a42f5c62f02e5b5a26f1d218f5
Peter John Arao <pitzzahh@gmail.com>
2025-03-26 21:12:09 +0800

    feat: add SendEmailToParticipantsDialog component export

--- src/routes/events/(components)/(participant)/index.ts
+++ src/routes/events/(components)/(participant)/index.ts
@@ -1,1 +1,2 @@
 export { default as ImportParticipantDialog } from './import-participant-dialog.svelte';
+export { default as SendEmailToParticipantsDialog } from './send-email-to-participants-dialog.svelte';
