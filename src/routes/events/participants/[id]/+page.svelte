bf38464ac688e61c4fef57c84a7c294d7fc93cee
Peter John Arao <pitzzahh@gmail.com>
2025-03-26 15:48:54 +0800

    feat: comment out QR code sending functionality in participant page

--- src/routes/events/participants/[id]/+page.svelte
+++ src/routes/events/participants/[id]/+page.svelte
@@ -551,22 +551,22 @@
 
 		return toast.warning('This feature is still in development');
 
-		email.send_qr_code_worker.postMessage(
-			JSON.stringify({
-				participants: getParticipantsWithQRCode(),
-				PLUNK_API: await getEnv('PLUNK_API'),
-				PLUNK_SK: await getEnv('PLUNK_SK'),
-				event_details: {
-					event_name: event_details.event_name,
-					event_date: event_details.start_date.toISOString(),
-					event_location: event_details.location
-				}
-			})
-		);
+		// email.send_qr_code_worker.postMessage(
+		// 	JSON.stringify({
+		// 		participants: getParticipantsWithQRCode(),
+		// 		PLUNK_API: await getEnv('PLUNK_API'),
+		// 		PLUNK_SK: await getEnv('PLUNK_SK'),
+		// 		event_details: {
+		// 			event_name: event_details.event_name,
+		// 			event_date: event_details.start_date.toISOString(),
+		// 			event_location: event_details.location
+		// 		}
+		// 	})
+		// );
 
-		toast.info(`Sending QR codes to participants`, {
-			description: 'This may take a few moments. You can continue using the application.'
-		});
+		// toast.info(`Sending QR codes to participants`, {
+		// 	description: 'This may take a few moments. You can continue using the application.'
+		// });
 	}
 
 	watch(
