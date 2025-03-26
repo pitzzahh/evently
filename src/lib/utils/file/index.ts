73b7502908815d3fdd0e321a01a5f6e7c574c261
Peter John Arao <pitzzahh@gmail.com>
2025-03-26 16:30:41 +0800

    feat: add dataURLtoFile function to convert data URLs to File objects

---
+++ src/lib/utils/file/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Converts a data URL string to a File object
+ * @param dataurl - The data URL string to convert
+ * @param filename - The name to give to the generated file
+ * @returns A File object created from the data URL
+ */
+export function dataURLtoFile(dataurl: string, filename: string) {
+  var arr = dataurl.split(','),
+    mime = arr[0].match(/:(.*?);/)?.[1] || 'application/octet-stream',
+    bstr = atob(arr[arr.length - 1]),
+    n = bstr.length,
+    u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new File([u8arr], filename, { type: mime });
+}
