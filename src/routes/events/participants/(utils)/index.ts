8ccfcaec4c963dd203f5e8418f919b613d4bfc53
Alessandro Benig <alebenig4@gmail.com>
2025-03-26 13:52:35 +0800

    feat: add late arrival duration calculations to attendance records and participant details

--- src/routes/events/participants/(utils)/index.ts
+++ src/routes/events/participants/(utils)/index.ts
@@ -1,47 +1,72 @@
-import type { AttendanceRecordCollection, ParticipantCollection } from "@/db/models";
-import type { ParticipantAttendance } from "@/db/models/types";
+import type {
+	AttendanceRecordCollection,
+	EventScheduleCollection,
+	ParticipantCollection
+} from '@/db/models';
+import type { EventSchedule, ParticipantAttendance } from '@/db/models/types';
+import { getTimeDifference } from '@routes/events/utils';
 
 export function getPopulatedAttendanceRecords(
-  event_id: string,
-  collections: {
-    attendance_records_collection: AttendanceRecordCollection;
-    participant_collection: ParticipantCollection;
-  },
-  current_event_day?: string
+	event_id: string,
+	collections: {
+		attendance_records_collection: AttendanceRecordCollection;
+		participant_collection: ParticipantCollection;
+		event_schedules_collection: EventScheduleCollection;
+	},
+	current_event_day?: string
 ): ParticipantAttendance[] {
-  const attendance_records = collections.attendance_records_collection
-    .find({
-      event_id: event_id,
-      ...(current_event_day && { day: current_event_day })
-    })
-    .fetch();
+	const event_schedules = collections.event_schedules_collection
+		.find({
+			event_id
+		})
+		.fetch();
 
-  const participantIds = [...new Set(attendance_records.map((record) => record.participant_id))];
+	const attendance_records = collections.attendance_records_collection
+		.find({
+			event_id: event_id,
+			...(current_event_day && { day: current_event_day })
+		})
+		.fetch();
 
-  const participants = collections.participant_collection
-    .find({
-      id: { $in: participantIds }
-    })
-    .fetch();
+	const participantIds = [...new Set(attendance_records.map((record) => record.participant_id))];
 
-  const participant_map = new Map(
-    participants.map((participant) => [participant.id, participant])
-  );
+	const participants = collections.participant_collection
+		.find({
+			id: { $in: participantIds }
+		})
+		.fetch();
 
-  // return new combined objects without modifying originals
-  return attendance_records
-    .map((record) => {
-      const participant = participant_map.get(record.participant_id);
-      return {
-        ...record,
-        first_name: participant?.first_name,
-        middle_name: participant?.middle_name,
-        last_name: participant?.last_name,
-        email: participant?.email,
-        participant
-      };
-    })
-    .sort(
-      (a, b) => (b.latest_time_scanned?.getTime() ?? 0) - (a.latest_time_scanned?.getTime() ?? 0)
-    ) as ParticipantAttendance[];
+	const participant_map = new Map(participants.map((participant) => [participant.id, participant]));
+
+	// return new combined objects without modifying originals
+	return attendance_records
+		.map((record) => {
+			const participant = participant_map.get(record.participant_id);
+			const matching_event_day_sched = event_schedules.find(
+				(sched) => sched.day.toString() === record.day
+			) as EventSchedule;
+			const late_am_time_in_duration =
+				record?.am_time_in && matching_event_day_sched.am_start
+					? getTimeDifference(record?.am_time_in, matching_event_day_sched.am_start)
+					: undefined;
+
+			const late_pm_time_in_duration =
+				record?.pm_time_in && matching_event_day_sched.pm_start
+					? getTimeDifference(record?.pm_time_in, matching_event_day_sched.pm_start)
+					: undefined;
+
+			return {
+				...record,
+				first_name: participant?.first_name,
+				middle_name: participant?.middle_name,
+				last_name: participant?.last_name,
+				email: participant?.email,
+				participant,
+				late_am_time_in_duration,
+				late_pm_time_in_duration
+			};
+		})
+		.sort(
+			(a, b) => (b.latest_time_scanned?.getTime() ?? 0) - (a.latest_time_scanned?.getTime() ?? 0)
+		) as ParticipantAttendance[];
 }
