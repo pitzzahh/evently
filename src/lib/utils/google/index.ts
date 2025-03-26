42154959213e19cefb3e736a6e365f69835fea6e
Peter John Arao <pitzzahh@gmail.com>
2025-03-26 14:33:34 +0800

    feat: enhance getGoogleAuthClient to validate Google credentials before usage

--- src/lib/utils/google/index.ts
+++ src/lib/utils/google/index.ts
@@ -1,10 +1,14 @@
 import { google } from 'googleapis';
 
 export async function getGoogleAuthClient(
-  client_email?: string,
-  client_id?: string,
-  private_key?: string,
+  client_email?: string | null,
+  client_id?: string | null,
+  private_key?: string | null,
 ) {
+
+  if (!client_email || !client_id || !private_key) {
+    throw new Error('Google credentials are not provided');
+  }
 
   return new google.auth.GoogleAuth({
     credentials: {
