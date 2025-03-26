199843997017ae79c913eac63fefabf600aaea6e
Peter John Arao <pitzzahh@gmail.com>
2025-03-26 09:59:26 +0800

    refactor: update uploadFileToGoogleDrive function to use descriptive parameter names

--- src/lib/utils/google/drive/index.ts
+++ src/lib/utils/google/drive/index.ts
@@ -3,21 +3,20 @@ import { google } from 'googleapis';
 
 export async function uploadFileToGoogleDrive(auth: GoogleAuth, data: {
   file: Blob,
-  mimeType: 'image/bmp' | 'image/jpeg' | 'image/x-png'
+  file_name: string,
+  mime_type: 'image/bmp' | 'image/jpeg' | 'image/x-png'
 }) {
   const drive = google.drive({ version: 'v3', auth });
-  const fileMetadata = {
-    name: 'My File',
-    mimeType: data.mimeType
-  };
-  const media = {
-    mimeType: data.mimeType,
-    body: data.file
-  };
   try {
     const response = await drive.files.create({
-      requestBody: fileMetadata,
-      media,
+      requestBody: {
+        name: data.file_name,
+        mimeType: data.mime_type
+      },
+      media: {
+        mimeType: data.mime_type,
+        body: data.file
+      },
       fields: 'id'
     });
     console.log('File uploaded successfully:', response.data.id);
