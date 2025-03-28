658c09dba9e732c9e9ff3827e610e16bed2ce293
Alessandro Benig <alebenig4@gmail.com>
2025-03-28 12:50:14 +0800

    feat: update event details structure and enhance event color assignment in calendar component

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	import { EventCalendar } from '@/components/custom/event-calendar';
 	import { scale } from 'svelte/transition';
-	import type { EventDetails } from '@/db/models/types';
 	import { watch } from 'runed';
 	import { COLLECTIONS } from '@/db';
-	import moment from 'moment';
-	import { momentLocalizer, type Event as CalendarEvent } from 'react-big-calendar';
+	import type { CalendarEvent } from '@/components/custom/react-event-calendar/components';
+	import { checkEventStatus } from '@routes/events/utils';
 
 	interface ComponentState {
 		event_details: CalendarEvent[];
@@ -15,18 +14,26 @@
 		event_details: []
 	});
 
+	const event_colors = ['sky', 'amber', 'violet', 'rose', 'emerald', 'orange'] as const;
+
 	watch([() => COLLECTIONS.EVENT_DETAILS_COLLECTION.isLoading], () => {
 		const event_details_cursor = COLLECTIONS.EVENT_DETAILS_COLLECTION.find(
 			{},
 			{ fieldTracking: true }
 		);
 		comp_state.event_details = event_details_cursor.fetch().map((e) => {
-			const _event: CalendarEvent = {
+			return {
+				id: e.id,
 				title: e.event_name,
+				description: e.description,
 				start: e.start_date,
-				end: e.end_date
+				end: e.end_date,
+				location: e.location,
+				color: event_colors.at(Math.random() * event_colors.length),
+				eventImage: e.cover,
+				eventStatus: checkEventStatus(e.start_date, e.end_date),
+				eventDuration: e.difference_in_days
 			};
-			return _event;
 		});
 
 		$inspect(comp_state.event_details);
@@ -35,6 +42,6 @@
 </script>
 
 <h2 class="mb-4 text-4xl font-semibold">Calendar of Events</h2>
-<div in:scale class="min-h-dvh bg-background p-4">
-	<EventCalendar localizer={momentLocalizer(moment)} events={comp_state.event_details} />
+<div in:scale class="min-h-dvh p-4">
+	<EventCalendar events={comp_state.event_details} />
 </div>
