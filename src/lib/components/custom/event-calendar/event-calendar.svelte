1477957e6bae150cec85b957e82dc7c375a12bb5
Alessandro Benig <alebenig4@gmail.com>
2025-03-28 12:51:24 +0800

    refactor: simplify event calendar component by updating imports and removing unused code

--- src/lib/components/custom/event-calendar/event-calendar.svelte
+++ src/lib/components/custom/event-calendar/event-calendar.svelte
@@ -2,23 +2,15 @@
 	import { createElement } from 'react';
 	import { createRoot } from 'react-dom/client';
 	import './event-calendar.css';
-	import EventCalendarComp from '../react-event-calendar/components/event-calendar-component';
+	import { EventCalendar } from '../react-event-calendar/components';
+	import type { EventCalendarProps } from '../react-event-calendar/components/event-calendar';
 
-	import type { CalendarProps, Event as CalendarEvent } from 'react-big-calendar';
-	let props: CalendarProps<object, object> = $props();
+	let props: EventCalendarProps = $props();
 	let rootEl: HTMLElement;
 
 	$effect(() => {
 		const root = createRoot(rootEl);
-
-		// import('react-big-calendar').then(({ Calendar }) => {
-		// 	const event_calendar = createElement(Calendar, {
-		// 		...props
-		// 	});
-		// 	root.render(event_calendar);
-		// });
-
-		const event_calendar = createElement(EventCalendarComp, {
+		const event_calendar = createElement(EventCalendar, {
 			...props
 		});
 		root.render(event_calendar);
@@ -27,8 +19,4 @@
 	});
 </script>
 
-<div bind:this={rootEl} class="root"></div>
-
-<style>
-	
-</style>
+<div bind:this={rootEl}></div>
